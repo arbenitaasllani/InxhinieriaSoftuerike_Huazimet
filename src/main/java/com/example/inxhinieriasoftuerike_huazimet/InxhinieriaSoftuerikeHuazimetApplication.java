b1042c0d2e6c81b2f5499dc8eaa0bbf74bbe9f73
Arbenita Asllani <arbenite.asllani@student.uni-pr.edu>
2023-11-29 19:18:41 +0100

    Initial commit

---
+++ src/main/java/com/example/inxhinieriasoftuerike_huazimet/InxhinieriaSoftuerikeHuazimetApplication.java
@@ -0,0 +1,13 @@
+package com.example.inxhinieriasoftuerike_huazimet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InxhinieriaSoftuerikeHuazimetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InxhinieriaSoftuerikeHuazimetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inxhinieriasoftuerike_huazimet/InxhinieriaSoftuerikeHuazimetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inxhinieriasoftuerike_huazimet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InxhinieriaSoftuerikeHuazimetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
